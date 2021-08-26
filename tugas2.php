622b4cee09bccaeab14b31c5be4393cded3ae9fd
Ilmam Fauzi <ilmamfauzi@gmail.com>
2021-08-26 22:34:05 +0700

    add file tugas2 & tugas2_2

---
+++ tugas2.php
@@ -0,0 +1,11 @@
+<?php
+$hari1 = 36;
+$pekerja1 = 12;
+$pekerja2 = 24;
+
+$hari2 = $hari1 / 2;
+
+echo "$hari1 Hari = $pekerja1 Pekerja<br>";
+echo "X Hari = $pekerja2 Pekerja<br>";
+echo "X = ...<br>";
+echo "Jawaban = $hari2 Hari";
